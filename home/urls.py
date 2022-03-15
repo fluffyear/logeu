5a030e6884ace7b00f3149b1dfc21086e9b2a581
James <jamesklogan06@gmail.com>
2022-03-15 22:20:25 +0000

    remove last trace of v_answer

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('nouns/', views.nouns),
-    path('verbs/answer/', views.v_answer),
     path('verbs/', views.verbs)
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from home.RanP import funx as funx
 import random
-word_answer = ''
 
 
 def index(request):
@@ -21,8 +20,6 @@ def verbs(request):
     choix = random.choice(list(dct.keys()))
     context['ans_greek'] = dct[choix][0]
     context['ans_word'] = dct[choix][1]
-    global word_answer
-    word_answer = dct[choix][0]
     for num, value in enumerate(dct.values(), start=1):
         context['word' + str(num)] = value[0]
     return render(request, 'verbs.html', context)
