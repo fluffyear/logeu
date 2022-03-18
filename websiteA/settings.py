b15c814534d526bf0501477f287f3cc4bcb50f3b
James <jamesklogan06@gmail.com>
2022-03-18 20:43:02 +0000

    update

--- websiteA/settings.py
+++ websiteA/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'logeu.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'logeu.herokuapp.com', 'www.logeu.cyou']
 
 
 # Application definition
